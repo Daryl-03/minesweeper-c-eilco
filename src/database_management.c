629859117063d7fc5853443fe537ece70169daf7
Toudonou <hnounagnonoswald@gmail.com>
2023-11-08 19:19:16 +0100

    limit the number of backups to 10

--- src/database_management.c
+++ src/database_management.c
@@ -5,7 +5,7 @@
 
 void save_game(Game *game){
     FILE *file, *tmpFile;
-    char *fileName = game->over ? file_games_completed: file_games_non_completed, ch;
+    char *fileName = game->over ? file_games_completed: file_games_non_completed, chaine[500], ch;
     int nbrLignes = 1;
 
     file = fopen(fileName, "a+");
@@ -16,7 +16,10 @@ void save_game(Game *game){
         if (ch == '\n') nbrLignes++;
     }
 
-    game->id = nbrLignes;
+    nbrLignes = nbrLignes >= 10 ? 9 : nbrLignes;
+    game->id = nbrLignes--;
+    for(int i = 0; i < strlen(game->name); i++)
+        if(game->name[i] == ' ') game->name[i] = '|';
     fprintf(tmpFile, "{%d;%s ;%d-%d;%d;%d;%d;%d;%d}", game->id, game->name, game->grid.size.width, game->grid.size.height, game->mines, game->flags, game->revealed, game->score, game->over);
 
     for(int i = 0; i < game->grid.size.height; i++){
@@ -27,8 +30,9 @@ void save_game(Game *game){
     fprintf(tmpFile, "\n");
 
     rewind(file);
-    while ((ch = fgetc(file)) != EOF) {
-        fputc(ch, tmpFile);
+    while ((fgets(chaine, 500, file)) != NULL && nbrLignes >= 1) {
+        chaine[1] = '0' + nbrLignes--;
+        fputs(chaine, tmpFile);
     }
     fclose(file);
     fclose(tmpFile);
@@ -49,6 +53,9 @@ Game *load_game(int id){
         fscanf(file, "{%d;%s ;%d-%d;%d;%d;%d;%d;%d}", &game->id, game->name, &game->grid.size.width, &game->grid.size.height, &game->mines, &game->flags, &game->revealed, &game->score, &over);
         game->over = over;
         if(game->id == id){
+             for(int i = 0; i < strlen(game->name); i++)
+                if(game->name[i] == '|') game->name[i] = ' ';
+
             game->grid.grid = (Cell**) malloc(game->grid.size.height * sizeof(Cell*));
             if(!game->grid.grid) exit(EXIT_FAILURE);
 
